58661560d21f245d3f659d823d9f60ab70c212d8
Ariane Hine <aah4@st-andrews.ac.uk>
2021-05-13 10:53:45 +0100

    Initial Commit

---
+++ PIG/PIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIGApp.swift
+//  PIG
+//
+//  Created by ariane hine on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct PIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
