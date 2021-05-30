b9ba488e7fa5472cd71ffc93b8a1302269acfee2
Ariane Hine <aah4@st-andrews.ac.uk>
2021-05-30 17:35:40 +0100

    adding some bits to improve view

--- TipsView.swift
+++ TipsView.swift
@@ -11,7 +11,27 @@ struct TipsView: View {
     @StateObject var statsController = StatsDataController();
     @Binding var worstArea: String
     var body: some View {
-        Text("Tips to improve \(worstArea) :) ")
+        Text("Tips to improve \(worstArea) :) ");
+        whatToImprove(worstArea: worstArea)
+        
+        
+    }
+}
+
+@ViewBuilder func whatToImprove(worstArea: String)-> some View{
+    switch worstArea {
+    case "Transport":
+        Text("Transport")
+    case "Food":
+        Text("Food")
+    case "Home":
+        Text("Home")
+    case "Fashion":
+        Text("Fashion")
+    case "Health":
+        Text("Health")
+    default:
+        Text("Else")
     }
 }
 
