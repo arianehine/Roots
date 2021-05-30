7e2169a14ad97ad504c3a456acbfc779afb3e607
Ariane Hine <aah4@st-andrews.ac.uk>
2021-05-30 21:37:26 +0100

    display partial earths now

--- NumberEarthsView.swift
+++ NumberEarthsView.swift
@@ -13,20 +13,51 @@ struct NumberEarthsView: View {
     //    @Binding var reports: [Report]
     //    @Binding var originalReports: [Report]
     @EnvironmentObject var statsController: StatsDataController
-    @State var numEarths : Int = 7;
+    @State var numEarths : Double = 7.75;
+    @State var numEarthsInt = 0;
     var body: some View {
         VStack{
-            Text("If everyone were to live like you, we would need \(numEarths) Earths").font(.title).frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).multilineTextAlignment(.center)
-            WrappingHStack(0..<numEarths, id:\.self, alignment: .center) { _ in
-                Image("logo")
-                    .resizable()
-                    .frame(width: 100, height: 89, alignment: .center)
+            
+            let numEarthsInt = Int(ceil(numEarths));
+            Text("If everyone were to live like you, we would need \(numEarths, specifier: "%.2f") Earths").font(.title).frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).multilineTextAlignment(.center)
+            WrappingHStack(0..<numEarthsInt, id:\.self, alignment: .center) { index in
+                Group{
+                    
+             
+                
+                    if ( index == numEarthsInt-1){
+                checkIfPartial(numEarths: numEarths, numEarthsInt: numEarthsInt)
+                    }else{
+                        
+                       Image("logo")
+                           .resizable()
+                           .frame(width: 100, height: 89, alignment: .center);
+                       
+                      
+                    }
+               
+                }
             }
             
             
         }
        
     }
+}
+
+@ViewBuilder func checkIfPartial(numEarths: Double, numEarthsInt: Int)-> some View{
+    
+    if Double(numEarthsInt) > numEarths {
+        let remainder1 = numEarths.truncatingRemainder(dividingBy: 1)
+        
+        Image("logo")
+                       .resizable()
+                       .frame(width: 100, height: 89)
+            .mask(Rectangle().padding(.trailing, 100 * CGFloat(1 - remainder1))) //<-- Here
+    }else{
+        Text("no")
+    }
+
 }
 
 struct NumberEarthsView_Previews: PreviewProvider {
